bfe96bbb5efe03cb3f619f88453ddf5b7d4bc562
Roberto Araneda Espinoza <robaraneda@gmail.com>
2022-03-12 15:14:00 -0300

    add create and get all user service test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService;
+  let userRepository;
+  const mockUserRepository = () => ({
+    createUser: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
-
-    service = module.get<UserService>(UserService);
+    userService = await module.get<UserService>(UserService);
+    userRepository = await module.get<UserRepository>(UserRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createUser', () => {
+    it('should save a user in the database', async () => {
+      userRepository.createUser.mockResolvedValue('someUser');
+      expect(userRepository.createUser).not.toHaveBeenCalled();
+      const createUserDto = {
+        name: 'sample name',
+        lastname: 'sample lastname',
+        email: 'sample email',
+        password: 'sample password',
+      };
+      const result = await userService.createUser(createUserDto);
+      expect(userRepository.createUser).toHaveBeenCalledWith(createUserDto);
+      expect(result).toEqual('someUser');
+    });
+  });
+
+  describe('getUsers', () => {
+    it('should get all users', async () => {
+      userRepository.find.mockResolvedValue('someUsers');
+      expect(userRepository.find).not.toHaveBeenCalled();
+      const result = await userService.getUsers();
+      expect(userRepository.find).toHaveBeenCalled();
+      expect(result).toEqual('someUsers');
+    });
   });
 });
